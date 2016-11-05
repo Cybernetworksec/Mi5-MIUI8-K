336706866708058bac930c062ce610cf3cba2a5e
Shaky <shaky153@gmail.com>
2016-11-05 08:10:10 +0000

    MIUI8 kernel based on LA.HB.1.3.2

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 123 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#123 SMP PREEMPT Fri Nov 4 19:13:48 GMT 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "shaky156-MS-7816"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (Linaro GCC 4.9-2015.06) "
